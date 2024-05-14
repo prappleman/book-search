0550d7eb7fd2d06d2758b94c2fd0160538508612
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-13 22:49:34 -0600

    updated server file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// Start Apollo Server
+server.listen(PORT).then(({ url }) => {
+  console.log(`🌍 API server now listening on ${url}`);
 });
