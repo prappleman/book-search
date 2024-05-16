ecb37845fb5604b966e4eaf1ed0098484c448735
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-15 19:11:20 -0600

    upated to use mutations and queries and also removed unused code

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,22 +1,19 @@
-import { useState, useEffect } from 'react';
-import {
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  Row
-} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { SAVE_BOOK } from "../utils/mutations";
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from "@apollo/client";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
@@ -72,9 +69,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
+      const response = await saveBook({ variables: bookToSave, token });
+      
+      if (!response) {
         throw new Error('something went wrong!');
       }
 
