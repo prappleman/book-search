b6384e86b19b5b46f8b9014f273882ccea47abf5
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-15 21:39:17 -0600

    aded start server function and adjusted connection file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
+const {  authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -22,7 +22,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// Start Apollo Server
-server.listen(PORT).then(({ url }) => {
-  console.log(`🌍 API server now listening on ${url}`);
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+  
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
+
