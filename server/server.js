4e69b288d2c6778d1149117f81bd41776e61b3da
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-15 19:30:31 -0600

    updated json packages and switch port to 3001

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
