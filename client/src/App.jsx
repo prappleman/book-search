118683861fc3385d7d447ca88295c7e33b0db385
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-16 23:07:08 -0600

    upated uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, ApolloProvider, InMemoryCache, } from "@apollo/client";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  uri: 'https://book-search-83k4.onrender.com/graphql',
   cache: new InMemoryCache(),
 });
 
