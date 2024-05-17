07201e32445b77395141625716733a3b3b55e732
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-16 22:38:00 -0600

    removed unused code

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -46,7 +46,7 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {userData.savedBooks && userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -8,22 +8,16 @@ import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from "@apollo/client";
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   const [saveBook] = useMutation(SAVE_BOOK);
 
-
-// set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
+    saveBookIds(savedBookIds); // Save IDs to localStorage on update
   }, [savedBookIds]);
 
-  // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -55,13 +49,9 @@ const SearchBooks = () => {
     }
   };
 
-  // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    // get token
-    const token = Auth.getToken();
+    const token = Auth.getToken(); // Get the authentication token
 
     if (!token) {
       console.error('You need to be logged in to save books.');
@@ -75,7 +65,6 @@ const SearchBooks = () => {
         throw new Error('Something went wrong while saving the book.');
       }
 
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
