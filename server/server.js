a6512878c32c32b6719f2cd53c554807363c6e73
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-16 18:51:48 -0600

    updated build

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const {  authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const db = require('./config/connection');
@@ -17,9 +17,12 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 const startApolloServer = async (typeDefs, resolvers) => {
@@ -30,10 +33,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
+    });
+  });
 };
-  
+
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
-
