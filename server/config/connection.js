1907f08eece43578b52e70422670f23d38187a6f
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-20 21:32:00 -0600

    rearranged mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 
+// Define the connection URI
+const uri = 'mongodb+srv://parkerrappleye1:Parker4780@ac-gbilfhr.ppokqij.mongodb.net/mydatabase?retryWrites=true&w=majority';
+
+// Function to connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://parkerrappleye1:Parker4780@book-search.ppokqij.mongodb.net/', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
-  console.log('MongoDB connected');
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
   } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit process with failure
   }
 };
 
+// Export the connection function
 module.exports = connectDB;
