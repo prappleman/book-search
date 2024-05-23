9c1d2647d3bec79e518cc89f3559c4cbac832a23
parkerrappleye <parker.rappleye1@gmail.com>
2024-05-22 20:38:57 -0600

    updated build and connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Define the connection URI
-const uri = 'mongodb+srv://parkerrappleye1:Parker4780@book-search.ppokqij.mongodb.net/book-search';
+const uri = 'mongodb+srv://parkerrappleye1:Parker4780@book-search.ppokqij.mongodb.net/?retryWrites=true&w=majority&appName=book-search';
 
 // Function to connect to MongoDB
 const connectDB = async () => {
